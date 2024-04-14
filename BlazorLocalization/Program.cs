0f2ee555aed611227f0517a285a597a4e5bb89ac
Macoratti .net <macoratti@yahoo.com>
2024-04-14 09:53:46 -0300

    Add project files.

---
+++ BlazorLocalization/Program.cs
@@ -0,0 +1,40 @@
+using BlazorLocalization.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization();
+builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+string[] supportedCultures = ["pt-BR", "en-US", "es-ES"];
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapControllers();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
